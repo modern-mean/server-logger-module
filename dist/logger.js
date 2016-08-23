18ea5591b9dc4361af569252c4c4ff4803b1f171
Andrew Throener <athroener@gmail.com>
2016-08-23 17:40:24 -0500

    Update to es6 ext

--- dist/logger.js
+++ dist/logger.js
@@ -1,32 +1,24 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.MMLogger = undefined;
+import winston from 'winston';
 
-var _winston = require('winston');
-
-var _winston2 = _interopRequireDefault(_winston);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-class MMLogger {
+export class MMLogger {
 
   constructor(config) {
     this.transports = [];
     if (config.winston.file !== 'false') {
-      this.transports.push(new _winston2.default.transports.File({ filename: config.winston.file }));
+      this.transports.push(new (winston.transports.File)({ filename: config.winston.file }));
     }
 
     if (config.winston.console === 'true') {
-      this.transports.push(new _winston2.default.transports.Console());
+      this.transports.push(new (winston.transports.Console)());
     }
 
-    this.logger = new _winston2.default.Logger({
+    this.logger = new (winston.Logger)({
       level: config.winston.level,
       transports: this.transports
     });
+
   }
 
   get() {
@@ -34,4 +26,3 @@ class MMLogger {
   }
 
 }
-exports.MMLogger = MMLogger;
