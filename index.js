0e252b0f5b281c8f79e39c0dde37272c8c8cdcb1
Andrew Throener <athroener@gmail.com>
2016-08-24 10:52:48 -0500

    bump

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _logger = require('./dist/logger');
+var _module = require('./dist/module');
 
-Object.keys(_logger).forEach(function (key) {
+Object.keys(_module).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
   Object.defineProperty(exports, key, {
     enumerable: true,
     get: function () {
-      return _logger[key];
+      return _module[key];
     }
   });
 });
